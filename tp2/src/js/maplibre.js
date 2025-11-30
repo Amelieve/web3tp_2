f376f461192324b343945f406aaf55753327a909
Amelieve <2496667@cmontmorency.qc.ca>
2025-11-30 12:19:43 -0500

    map

--- tp2/src/js/maplibre.js
+++ tp2/src/js/maplibre.js
@@ -1,11 +1,33 @@
-import maplibregl from "maplibre-gl";
+const styleBase = {
+  version: 8,
+  sources: {
+    osm: {
+      type: "raster",
+      tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
+      tileSize: 256
+    }
+  },
+  layers: [
+    {
+      id: "osm-layer",
+      type: "raster",
+      source: "osm",
+      paint: {
+        "raster-opacity": 1
+      }
+    }
+  ]
+};
 
 const carte = new maplibregl.Map({
-  container: "map",                  
-  style: "https://demotiles.maplibre.org/style.json",
-  center: [9.1999174, 45.9652533],  
+  container: "map",
+  style: styleBase,
+  center: [9.202387,45.965105],
+  zoom: 17
 });
 
 new maplibregl.Marker()
-  .setLngLat([9.1999174, 45.9652533])   
+  .setLngLat([9.202387,45.965105])
   .addTo(carte);
+
+
